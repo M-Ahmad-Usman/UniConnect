c40f89bf968bffd58abeb6423d3902726f2fe759
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-03-04 03:09:38 +0500

    Use separate vars for db connection.

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -3,8 +3,14 @@ import { Pool } from 'pg'
 import { env } from '../config/env.js'
 import type { Database } from './types.js'
 
-export const db = new Kysely<Database>({
-  dialect: new PostgresDialect({
-    pool: new Pool({ connectionString: env.DATABASE_URL }),
+const dialect = new PostgresDialect({
+  pool: new Pool({
+    database: env.POSTGRES_DB,
+    host: env.HOST,
+    user: env.POSTGRES_USER,
+    port: env.DB_PORT,
+    max: 10,
   }),
 })
+
+export const db = new Kysely<Database>({ dialect })
