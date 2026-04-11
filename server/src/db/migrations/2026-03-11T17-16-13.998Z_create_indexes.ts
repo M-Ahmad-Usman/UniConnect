e28f17fef349295480a4d81c755618a2bd10441d
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-11 23:10:16 +0500

    refactor naming in create_indexes migration.
    - add `onTable` key to simplify the table name retrieval for index creation
    - update `columns` to `onColumns` for better clarity
    - rename `TABLE_INDEXES` to `INDEXES`
    - keep `TABLE_NAMES` as a type-only import for typing

--- server/src/db/migrations/2026-03-11T17-16-13.998Z_create_indexes.ts
+++ server/src/db/migrations/2026-03-11T17-16-13.998Z_create_indexes.ts
@@ -1,5 +1,5 @@
 import type { Kysely, Expression, SqlBool } from 'kysely'
-import { TABLE_NAMES } from './2026-03-07T02-23-50.616Z_create_tables.js'
+import type { TABLE_NAMES } from './2026-03-07T02-23-50.616Z_create_tables.js'
 import { sql } from 'kysely'
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -11,17 +11,15 @@ import { sql } from 'kysely'
 
 export async function up(db: Kysely<any>): Promise<void> {
 
-  for (const [camelCasedTableName, indexes] of Object.entries(TABLE_INDEXES)) {
-
-    const snakeCasedTableName = TABLE_NAMES[camelCasedTableName as keyof typeof TABLE_NAMES]
-
+  for (const indexes of Object.values(INDEXES)) {
     for (const index of Object.values(indexes)) {
       let query = db.schema
         .createIndex(index.name)
-        .on(snakeCasedTableName)
-        .columns(index.columns)
+        .on(index.onTable)
+        .columns(index.onColumns)
 
-      if (index.where) query = query.where(index.where)
+      if (index.where)
+        query = query.where(index.where)
 
       await query.execute()
     }
@@ -30,7 +28,7 @@ export async function up(db: Kysely<any>): Promise<void> {
 
 export async function down(db: Kysely<any>): Promise<void> {
 
-  for (const indexes of Object.values(TABLE_INDEXES))
+  for (const indexes of Object.values(INDEXES))
     for (const index of Object.values(indexes))
       await db.schema
         .dropIndex(index.name)
@@ -41,8 +39,10 @@ export async function down(db: Kysely<any>): Promise<void> {
 
 // Types and constants
 interface Index {
+  // name should follow the pattern: idx_{table_name}_{column_names_joined_by_underscore}
   name: string
-  columns: string[]
+  onTable: typeof TABLE_NAMES[keyof typeof TABLE_NAMES]
+  onColumns: string[]
   where?: Expression<SqlBool>
 }
 
@@ -51,7 +51,7 @@ type TableIndexes = Partial<Record<keyof typeof TABLE_NAMES, Record<string, Inde
 /**
  * Centralized performance index definitions (Single Source of Truth).
  */
-const TABLE_INDEXES: TableIndexes = {
+const INDEXES: TableIndexes = {
 
   /**
    * constraints (indexes) already defined in create_tables
@@ -74,11 +74,13 @@ const TABLE_INDEXES: TableIndexes = {
     */
     onDepartmentId: {
       name: 'idx_programs_department_id',
-      columns: ['department_id'],
+      onTable: 'programs',
+      onColumns: ['department_id'],
     },
     onProgramDirectorId: {
       name: 'idx_programs_program_director_id',
-      columns: ['program_director_id'],
+      onTable: 'programs',
+      onColumns: ['program_director_id'],
     },
   },
 
@@ -96,7 +98,8 @@ const TABLE_INDEXES: TableIndexes = {
   users: {
     onPublicId: {
       name: 'idx_users_public_id',
-      columns: ['public_id'],
+      onTable: 'users',
+      onColumns: ['public_id'],
     },
   },
 
@@ -107,7 +110,8 @@ const TABLE_INDEXES: TableIndexes = {
   userTypeAssignments: {
     onType: {
       name: 'idx_user_type_assignments_type',
-      columns: ['type'],
+      onTable: 'user_type_assignments',
+      onColumns: ['type'],
     },
   },
 
@@ -118,14 +122,16 @@ const TABLE_INDEXES: TableIndexes = {
   students: {
     onClassId: {
       name: 'idx_students_class_id',
-      columns: ['class_id'],
+      onTable: 'students',
+      onColumns: ['class_id'],
     },
   },
 
   teachers: {
     onDepartmentId: {
       name: 'idx_teachers_department_id',
-      columns: ['department_id'],
+      onTable: 'teachers',
+      onColumns: ['department_id'],
     },
   },
 
@@ -138,7 +144,8 @@ const TABLE_INDEXES: TableIndexes = {
   classes: {
     onPublicId: {
       name: 'idx_classes_public_id',
-      columns: ['public_id'],
+      onTable: 'classes',
+      onColumns: ['public_id'],
     },
   },
 
@@ -150,22 +157,26 @@ const TABLE_INDEXES: TableIndexes = {
   societies: {
     onPublicId: {
       name: 'idx_societies_public_id',
-      columns: ['public_id'],
+      onTable: 'societies',
+      onColumns: ['public_id'],
       where: sql<boolean>`is_deleted = false`,
     },
     onDepartmentId: {
       name: 'idx_societies_department_id',
-      columns: ['department_id'],
+      onTable: 'societies',
+      onColumns: ['department_id'],
       where: sql<boolean>`is_deleted = false`,
     },
     onPresidentId: {
       name: 'idx_societies_president_id',
-      columns: ['president_id'],
+      onTable: 'societies',
+      onColumns: ['president_id'],
       where: sql<boolean>`is_deleted = false`,
     },
     onConvenorId: {
       name: 'idx_societies_convenor_id',
-      columns: ['convenor_id'],
+      onTable: 'societies',
+      onColumns: ['convenor_id'],
       where: sql<boolean>`is_deleted = false`,
     },
   },
@@ -173,12 +184,14 @@ const TABLE_INDEXES: TableIndexes = {
   servers: {
     onPublicId: {
       name: 'idx_servers_public_id',
-      columns: ['public_id'],
+      onTable: 'servers',
+      onColumns: ['public_id'],
       where: sql<boolean>`is_deleted = false`,
     },
     onType: {
       name: 'idx_servers_type',
-      columns: ['type'],
+      onTable: 'servers',
+      onColumns: ['type'],
       where: sql<boolean>`is_deleted = false`,
     },
   },
@@ -190,24 +203,28 @@ const TABLE_INDEXES: TableIndexes = {
   channels: {
     onPublicId: {
       name: 'idx_channels_public_id',
-      columns: ['public_id'],
+      onTable: 'channels',
+      onColumns: ['public_id'],
       where: sql<boolean>`is_deleted = false`,
     },
     onServerIdCourseId: {
       name: 'idx_channels_server_id_course_id',
-      columns: ['server_id', 'course_id'],
-      // course type is derived from seed lookup tables migration from 'channel_types' table
+      onTable: 'channels',
+      onColumns: ['server_id', 'course_id'],
+      // course type is derived from seed lookup tables migration from 'channel_types' onTable
       where: sql<boolean>`is_deleted = false AND type = 'course'`,
     },
     onServerIdProgramId: {
       name: 'idx_channels_server_id_program_id',
-      columns: ['server_id', 'program_id'],
-      // program type is derived from seed lookup tables migration from 'channel_types' table
+      onTable: 'channels',
+      onColumns: ['server_id', 'program_id'],
+      // program type is derived from seed lookup tables migration from 'channel_types' onTable
       where: sql<boolean>`is_deleted = false AND type = 'program'`,
     },
     onType: {
       name: 'idx_channels_type',
-      columns: ['type'],
+      onTable: 'channels',
+      onColumns: ['type'],
       where: sql<boolean>`is_deleted = false`,
     },
   },
@@ -219,7 +236,8 @@ const TABLE_INDEXES: TableIndexes = {
   serverMemberships: {
     onServerId: {
       name: 'idx_server_memberships_server_id',
-      columns: ['server_id'],
+      onTable: 'server_memberships',
+      onColumns: ['server_id'],
     },
   },
 
@@ -230,12 +248,14 @@ const TABLE_INDEXES: TableIndexes = {
   societyMembershipRequests: {
     onSocietyIdIsReviewed: {
       name: 'idx_society_membership_requests_society_id_is_reviewed',
-      columns: ['society_id', 'is_reviewed'],
+      onTable: 'society_membership_requests',
+      onColumns: ['society_id', 'is_reviewed'],
       where: sql<boolean>`is_reviewed = false`,
     },
     onUserId: {
       name: 'idx_society_membership_requests_user_id',
-      columns: ['user_id'],
+      onTable: 'society_membership_requests',
+      onColumns: ['user_id'],
     },
   },
 
@@ -246,7 +266,8 @@ const TABLE_INDEXES: TableIndexes = {
   courses: {
     onDepartmentId: {
       name: 'idx_courses_department_id',
-      columns: ['department_id'],
+      onTable: 'courses',
+      onColumns: ['department_id'],
     },
   },
 
@@ -257,29 +278,34 @@ const TABLE_INDEXES: TableIndexes = {
   courseAssignments: {
     onTeacherId: {
       name: 'idx_course_assignments_teacher_id',
-      columns: ['teacher_id'],
+      onTable: 'course_assignments',
+      onColumns: ['teacher_id'],
     },
   },
 
   posts: {
     onPublicId: {
       name: 'idx_posts_public_id',
-      columns: ['public_id'],
+      onTable: 'posts',
+      onColumns: ['public_id'],
       where: sql<boolean>`is_deleted = false`,
     },
     onChannelIdPriority: {
       name: 'idx_posts_channel_id_priority',
-      columns: ['channel_id', 'priority'],
+      onTable: 'posts',
+      onColumns: ['channel_id', 'priority'],
       where: sql<boolean>`is_deleted = false`,
     },
     onChannelIdCreatedAt: {
       name: 'idx_posts_channel_id_created_at',
-      columns: ['channel_id', 'created_at'],
+      onTable: 'posts',
+      onColumns: ['channel_id', 'created_at'],
       where: sql<boolean>`is_deleted = false`,
     },
     onChannelIdIsPinned: {
       name: 'idx_posts_channel_id_is_pinned',
-      columns: ['channel_id', 'is_pinned'],
+      onTable: 'posts',
+      onColumns: ['channel_id', 'is_pinned'],
       where: sql<boolean>`is_deleted = false`,
     },
   },
@@ -287,7 +313,8 @@ const TABLE_INDEXES: TableIndexes = {
   postAttachments: {
     onPostId: {
       name: 'idx_post_attachments_post_id',
-      columns: ['post_id'],
+      onTable: 'post_attachments',
+      onColumns: ['post_id'],
     },
   },
 
@@ -310,26 +337,31 @@ const TABLE_INDEXES: TableIndexes = {
   userRoleAssignments: {
     onServerId: {
       name: 'idx_user_role_assignments_server_id',
-      columns: ['server_id'],
+      onTable: 'user_role_assignments',
+      onColumns: ['server_id'],
     },
     onChannelId: {
       name: 'idx_user_role_assignments_channel_id',
-      columns: ['channel_id'],
+      onTable: 'user_role_assignments',
+      onColumns: ['channel_id'],
     },
   },
 
   notifications: {
     onPostIdUserId: {
       name: 'idx_notifications_post_id_user_id',
-      columns: ['post_id', 'user_id'],
+      onTable: 'notifications',
+      onColumns: ['post_id', 'user_id'],
     },
     onUserIdCreatedAt: {
       name: 'idx_notifications_user_id_created_at',
-      columns: ['user_id', 'created_at'],
+      onTable: 'notifications',
+      onColumns: ['user_id', 'created_at'],
     },
     onUnreadByUser: {
       name: 'idx_notifications_unread_by_user',
-      columns: ['user_id', 'created_at'],
+      onTable: 'notifications',
+      onColumns: ['user_id', 'created_at'],
       where: sql<boolean>`read_at IS NULL`,
     },
   },
@@ -343,7 +375,8 @@ const TABLE_INDEXES: TableIndexes = {
   refreshTokens: {
     onUserIdExpiresAtRevokedAt: {
       name: 'idx_refresh_tokens_user_id_expires_at_revoked_at',
-      columns: ['user_id', 'expires_at', 'revoked_at'],
+      onTable: 'refresh_tokens',
+      onColumns: ['user_id', 'expires_at', 'revoked_at'],
     },
   },
 
