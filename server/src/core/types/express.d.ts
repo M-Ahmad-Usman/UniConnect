d7c6b11fcb1c8c60180a041141d6c7f2fdba0bff
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-19 22:02:30 +0500

    Augment express response object.
    Add the following properties:
    - requestId: string
    - log: HttpLogger['logger']

---
+++ server/src/core/types/express.d.ts
@@ -0,0 +1,15 @@
+import type { HttpLogger } from 'pino-http'
+
+declare global {
+  namespace Express {
+    interface Request {
+      requestId: string
+      log: HttpLogger['logger']
+    }
+  }
+}
+
+// This empty export is required to make TypeScript treat this file as a
+// "module" rather than a "script". Without it, the 'declare global' block
+// may not work correctly in all TypeScript configurations.
+export {}
