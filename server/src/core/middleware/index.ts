6adf05696d997e6638283ed248687a29dc2686da
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-19 22:06:20 +0500

    Add index.ts in src/middleware/ for barrel import.

---
+++ server/src/core/middleware/index.ts
@@ -0,0 +1,5 @@
+import { errorHandler } from './errorHandler.js'
+import { notFoundHandler } from './notFound.js'
+import { validationHandler } from './validate.js'
+
+export default { errorHandler, notFoundHandler, validationHandler }
