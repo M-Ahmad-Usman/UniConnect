841725c88be5dee5c764c4a32cffb733077c815b
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-26 13:50:36 +0500

    Write middleware to validate content type.

---
+++ server/src/core/middleware/validateContentType.ts
@@ -0,0 +1,16 @@
+import { InvalidContentTypeError } from '../errors/AppError.js'
+
+import type { Request, Response, NextFunction } from 'express'
+import type { ContentType } from '../types/contentType.js'
+
+export const validateContentType = (contentType: ContentType) => {
+
+  return (request: Request, _response: Response, next: NextFunction) => {
+
+    if (!request.is(contentType)) {
+      next(new InvalidContentTypeError(contentType, 'Content format is not supported'))
+    }
+    next()
+  }
+
+}
