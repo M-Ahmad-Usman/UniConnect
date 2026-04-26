14d3677a2ac83f85d290f7b659e2f8a166a21642
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-26 13:45:45 +0500

    Move error types from core/errors to core/types.

---
+++ server/src/core/types/error.ts
@@ -0,0 +1,18 @@
+export type ErrorType =
+  // Generic
+  | 'INTERNAL_SERVER_ERROR'
+  | 'NOT_FOUND'
+  | 'BAD_REQUEST'
+
+  // Auth
+  | 'UNAUTHORIZED'
+  | 'FORBIDDEN'
+  | 'TOKEN_EXPIRED'
+  | 'TOKEN_INVALID'
+  | 'REFRESH_TOKEN_REUSE'
+
+  // Resources
+  | 'CONFLICT'
+
+  // Validation
+  | 'VALIDATION_FAILED'
