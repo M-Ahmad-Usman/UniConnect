d4c1f319994832ef01f320ac0807f53fe6ab1a5c
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-04-30 11:16:38 +0500

    Implement class repository.
    Add the following methods:
    - getIdFromPublicId(publicId: string): Promise<number | undefined>
    - getClassDepartmentServerId(classId: string): Promise<number | undefined>
    - getClassServerId(classId: string): Promise<number | undefined>
    - getStudentEnrollmentContext(publicId: string): Promise<{ classId: number; classServerId: number, departmentServerId: number } | undefined>

---
+++ server/src/modules/class/class.repository.ts
@@ -0,0 +1,64 @@
+import type { Kysely } from 'kysely'
+import type { Database } from '../../db/types.js'
+
+export default class ClassRepository {
+
+  constructor(private readonly db: Kysely<Database>) { }
+
+  async getIdFromPublicId(publicId: string, trx: Kysely<Database> = this.db): Promise<number | undefined> {
+    const classesRow = await trx.selectFrom('classes')
+      .select('classes.id')
+      .where('classes.publicId', '=', publicId)
+      .executeTakeFirst()
+
+    return classesRow?.id
+  }
+
+  /**
+   * Returns the id (serverId) of the class's department's server
+  **/
+  async getClassDepartmentServerId(classId: number, trx: Kysely<Database> = this.db): Promise<number | undefined> {
+    const classesProgramsJoinedRow = await trx.selectFrom('classes')
+      .innerJoin('programs', 'programs.id', 'classes.programId')
+      .innerJoin('departments', 'departments.id', 'programs.departmentId')
+      .select('departments.serverId')
+      .where('classes.id', '=', classId)
+      .executeTakeFirst()
+
+    return classesProgramsJoinedRow?.serverId
+  }
+
+  async getClassServerId(classId: number, trx: Kysely<Database> = this.db): Promise<number | undefined> {
+    const classesRow = await trx.selectFrom('classes')
+      .select('serverId')
+      .where('id', '=', classId)
+      .executeTakeFirst()
+
+    return classesRow?.serverId
+  }
+
+  /**
+   * Returns necessary data required for enrolling a new student
+   * Returns
+   * - class's id as classId
+   * - class's serverId as classServerId,
+   * - class's department's serverId as departmentServerId (serverId of department to which the class belongs)
+   */
+  async getStudentEnrollmentContext(publicId: string, trx = this.db): Promise<{
+    classId: number,
+    classServerId: number,
+    departmentServerId: number,
+  } | undefined> {
+    return trx
+      .selectFrom('classes')
+      .innerJoin('programs', 'programs.id', 'classes.programId')
+      .innerJoin('departments', 'departments.id', 'programs.departmentId')
+      .select([
+        'classes.id as classId',
+        'classes.serverId as classServerId',
+        'departments.serverId as departmentServerId',
+      ])
+      .where('classes.publicId', '=', publicId)
+      .executeTakeFirst()
+  }
+}
