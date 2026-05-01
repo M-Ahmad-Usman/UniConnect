242a1eb25c705d78e0c8e221418ccda5da4a9f1f
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-05-01 16:13:42 +0500

    Add createServer method in server.repository.ts

--- server/src/modules/server/server.repository.ts
+++ server/src/modules/server/server.repository.ts
@@ -1,9 +1,20 @@
 import type { Kysely } from 'kysely'
-import type { Database, InsertServerMembershipEntity } from '../../db/types.js'
+import type {
+  Database,
+  InsertServerEntity,
+  InsertServerMembershipEntity,
+} from '../../db/types.js'
 
 export default class ServerRepository {
 
   constructor(private readonly db: Kysely<Database>) { }
+
+  async createServer(serverDetails: InsertServerEntity, trx: Kysely<Database> = this.db) {
+    return await trx.insertInto('servers')
+      .values(serverDetails)
+      .returningAll()
+      .executeTakeFirstOrThrow()
+  }
 
   async addMember(membershipDetails: InsertServerMembershipEntity, trx: Kysely<Database> = this.db) {
 
