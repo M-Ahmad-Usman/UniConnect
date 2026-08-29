30607a437dcc5b097f6006b1e8902eaa380649ad
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-08-29 10:45:09 +0500

    Implement department repository interface.

--- server/src/modules/department/department.repository.ts
+++ server/src/modules/department/department.repository.ts
@@ -1,18 +1,19 @@
 import type { Kysely } from 'kysely'
 import type { Database, InsertDepartmentEntity } from '../../db/types.js'
+import type { IDepartmentRepository } from './department.interface.js'
 
-export default class DepartmentRepository {
+export default class DepartmentRepository implements IDepartmentRepository {
 
   constructor(private readonly db: Kysely<Database>) {}
 
-  async createDepartment(createDepartmentDetails: InsertDepartmentEntity, trx: Kysely<Database> = this.db) {
+  async createDepartment(departmentInsert: InsertDepartmentEntity, trx: Kysely<Database> = this.db) {
     return await trx.insertInto('departments')
-      .values(createDepartmentDetails)
+      .values(departmentInsert)
       .returningAll()
       .executeTakeFirstOrThrow()
   }
 
-  async getDepartmentServerId(departmentId: number, trx: Kysely<Database> = this.db) {
+  async findDepartmentServerId(departmentId: number, trx: Kysely<Database> = this.db) {
     const departmentsRow = await trx.selectFrom('departments')
       .select('serverId')
       .where('id', '=', departmentId)
@@ -20,5 +21,4 @@ export default class DepartmentRepository {
 
     return departmentsRow?.serverId
   }
-
 }
