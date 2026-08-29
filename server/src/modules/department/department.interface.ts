18f3a4a4ce45c62f80e83bea99d8638a060ce21a
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-08-29 10:42:25 +0500

    Write IDepartmentRepository interface.

---
+++ server/src/modules/department/department.interface.ts
@@ -0,0 +1,14 @@
+import type { Kysely } from 'kysely'
+import type { Database, DepartmentEntity, InsertDepartmentEntity } from '../../db/types.js'
+
+export interface IDepartmentRepository {
+  createDepartment(
+    departmentInsert: InsertDepartmentEntity,
+    trx?: Kysely<Database>
+  ): Promise<DepartmentEntity>,
+
+  findDepartmentServerId(
+    departmentId: number,
+    trx?: Kysely<Database>,
+  ): Promise<number | undefined>
+}
