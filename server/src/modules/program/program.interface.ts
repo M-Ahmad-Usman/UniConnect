a15bc25fe40519050fef1e92cfb29959fdd4cb2a
M-Ahmad-Usman <developer.ahmad@proton.me>
2026-08-30 16:29:14 +0500

    Write IProgramRepository in program.interface.ts

---
+++ server/src/modules/program/program.interface.ts
@@ -0,0 +1,31 @@
+import type { Kysely } from 'kysely'
+import type {
+  Database,
+  ProgramEntity,
+  InsertProgramEntity,
+  ProgramCurriculumEntity,
+  InsertProgramCurriculumEntity,
+} from '../../db/types.js'
+
+export interface IProgramRepository {
+
+  createProgram(
+    programInsert: InsertProgramEntity,
+    trx?: Kysely<Database>,
+  ): Promise<ProgramEntity>,
+
+  createProgramCurricula(
+    programCurriculaInsert: InsertProgramCurriculumEntity[],
+    trx?: Kysely<Database>,
+  ): Promise<ProgramCurriculumEntity[]>,
+
+  findProgramById(
+    id: number,
+    trx?: Kysely<Database>,
+  ): Promise<ProgramEntity | undefined>,
+
+  findSemesterCountById(
+    programId: number,
+    trx?: Kysely<Database>,
+  ): Promise<number | undefined>,
+}
